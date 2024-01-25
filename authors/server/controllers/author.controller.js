38ede1747b37616f1b038faa2b87ac185a919b3b
Seth Green <sethezgreen@gmail.com>
2024-01-25 15:12:11 -0500

    added read one, update, and delete functionality

--- authors/server/controllers/author.controller.js
+++ authors/server/controllers/author.controller.js
@@ -21,5 +21,38 @@ module.exports = {
             .catch((err) => {
                 res.status(200).json(err)
             })
+    },
+    
+    // Read One
+    findAuthor: (req, res) => {
+        Author.findOne({_id: req.params.id})
+            .then((author) => {
+                res.status(200).json(author)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
+    // Update Author
+    updateAuthor: (req, res) => {
+        Author.findOneAndUpdate({_id: req.params.id}, req.body, {new:true})
+            .then((updatedAuthor) => {
+                res.json(updatedAuthor)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
+    },
+
+    // Delete Author
+    deleteAuthor: (req, res) => {
+        Author.deleteOne({_id: req.params.id})
+            .then((result) => {
+                res.status(201).json(result)
+            })
+            .catch((err) => {
+                res.status(500).json(err)
+            })
     }
 }
--- authors/server/routes/author.routes.js
+++ authors/server/routes/author.routes.js
@@ -1,6 +1,10 @@
+const authorController = require('../controllers/author.controller')
 const AuthorController = require('../controllers/author.controller')
 
 module.exports = (app) => {
     app.post('/api/createAuthor', AuthorController.createAuthor)
     app.get('/api/authors', AuthorController.findAllAuthors)
+    app.get('/api/author/:id', AuthorController.findAuthor)
+    app.put('/api/updateAuthor/:id', AuthorController.updateAuthor)
+    app.delete('/api/deleteAuthor/:id', AuthorController.deleteAuthor)
 }
